9c2218bc78f01d588d53d68dbe72a8630178da33
shalini <shalini.v@tvsnext.io>
2021-08-23 11:36:56 +0530

    [shalini] added tracks and deleted

--- WebservicesforGenericOne/TestUsingGetEndPoint.cs
+++ WebservicesforGenericOne/TestUsingGetEndPoint.cs
@@ -14,19 +14,23 @@ namespace WebservicesforGenericOne
     public class TestUsingGetEndPoint
     {
         public string token = "";
-        public string userID = "";
-        public string playlistID = "";
+        public static string userID = "";
+        public static string playlistID = "";
+        public static string uri = "spotify:track:7mtYsNBYTDPa8Mscf166hg";
+       
+
         private static IRestResponse restResponse { get; set; }
         [TestInitialize]
         public void setup()
         {
-            token = "Bearer BQD8hjmOg-6DbRKSox_znXK3yIFg4-e4UkfOlbhWjD52QFtK96vQ42ib564XOWUUfnogh0t" +
-            "oF2PpmODMPoUnmSKCYqx42K26aTaQZZOZAEJV-N5PNbu78inbTNmH39CHS_PJ_zfwIZTTOV35mArI5kyr2wTrRP" +
-            "BROJ1vo9BYar-C43uusqYWE-9GMniNmgkmqw0jPYOpYL14gtTlTZaKXU4ZZXPZLZc8X3bA9Fm1356pRC6b67fC0" +
-            "GOh3HSq9wnvLj7ZLddwZ2JDQZu0rFK1UvM4HmfgNArTE60hpmoj";
+            token = "Bearer BQBnsuTmUcPcZaESycpuwPZlZZIkIg8tMyh7s4xK5t-JjLcmyTgoyupmWyyx86QNus3647y-n-Dgr" +
+                "uJVbBKLPwEXX9M2KdLYDWdmGCrGV7fLKNZbar_9mZSMVYUq_1e-bkX4tNys_nG3yVZ04McdPR3EqlpxHV18XDsQ2A" +
+                "f27CvvbQW6gduMAQJuMomtZskIn4WsTaCIcHs9-UPkEPfWElYpRPpJg93UAIS8XbGKK63v-I4M1GmIeNaPp5Tox0" +
+                "oqOWjkHnBuH9uPTjuVMrPBZuFtCNbgE403AM4KtOjM";
         }
+        [Priority(1)]
         [TestMethod]
-        public void get_CurrentUser()
+        public void A_getCurrentUser()
         {
 
             IRestClient restClient = new RestClient();
@@ -40,19 +44,21 @@ namespace WebservicesforGenericOne
             {
                 userID = d.id;
             }
-            Console.WriteLine(userID);
+            Console.WriteLine("Response: " + restResponse.Content);
+            Console.WriteLine("userID:"+userID);
         }
+        [Priority(2)]
         [TestMethod]
-        public void CreatePlaylist()
+        public void B_CreatePlaylist()
         {
-            get_CurrentUser();
+            A_getCurrentUser();
 
 
 
 
             string JsonData = "{" +
-                                    "\"name\": \"shalini Music Zone song's list\"," +
-                                    "\"description\": \"New playlist created\"," +
+                                    "\"name\": \"shalini's Music Zone song's list\"," +
+                                    "\"description\": \"New playlists created\"," +
                                     "\"public\" : \"false\"" +
                                     "}";
             IRestClient restClient = new RestClient();
@@ -79,19 +85,26 @@ namespace WebservicesforGenericOne
 
 
             }
-            Console.WriteLine(playlistID);
+            Console.WriteLine("Response: " + restResponse2.Content);
+            Console.WriteLine("PlaylistID:"+playlistID);
         }
 
-
+        [Priority(3)]
         [TestMethod]
-        public void ChangePlaylistDescription()
+        public void C_PlaylistDescriptionChanging()
         {
-            CreatePlaylist();
+            //CreatePlaylist();
+
+
+
             string json = "{" +
                             "\"name\": \"Updated Music Zone song's list\"," +
                             "\"description\": \"My playlist Created\"," +
                             "\"public\" : false" +
                             "}";
+
+
+
             IRestClient restClient = new RestClient();
             IRestRequest restRequest = new RestRequest("https://api.spotify.com/v1/playlists/" + playlistID + "/");
             restRequest.AddHeader("Authorization", "token" + token);
@@ -99,9 +112,52 @@ namespace WebservicesforGenericOne
             restRequest.AddHeader("content-Type", "application/json");
             restResponse = restClient.Put(restRequest);
             Console.WriteLine((int)restResponse.StatusCode);
-
-
         }
+        [Priority(4)]
+        [TestMethod]
+        public void D_TrackAdded()
+        {
+            //B_CreatePlaylist();
+
+
+
+            IRestClient restClient = new RestClient();
+            IRestRequest restRequest = new RestRequest("https://api.spotify.com/v1/playlists/" + playlistID + "/tracks?uris=" + uri);
+            restRequest.AddHeader("Authorization", "token" + token);
+            restRequest.AddParameter("uris", uri);
+            restResponse = restClient.Post(restRequest);
+            Console.WriteLine((int)restResponse.StatusCode);
+            Console.WriteLine(restResponse.Content);
+            
+        }
+        [Priority(5)]
+
+
+
+        [TestMethod]
+
+
+
+        public void TrackDeletion()
+        {
+            string json = "{ \"tracks\":" +
+                         "[{ \"uri\": \"spotify:track:7mtYsNBYTDPa8Mscf166hg\"}]}";
+
+
+
+            IRestClient restClient = new RestClient();
+            IRestRequest restRequest = new RestRequest("https://api.spotify.com/v1/playlists/" + playlistID + "/tracks");
+            restRequest.AddHeader("Authorization", "token" + token);
+            restRequest.AddJsonBody(json);
+            restResponse = restClient.Delete(restRequest);
+            Console.WriteLine((int)restResponse.StatusCode);
+        }
+
+
+
+
+
+
     }
 
 }
